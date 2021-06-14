23927fec348e3449babbc7a07c5e97548dc4f07f
Natalya <natalya.zapolsk@gmail.com>
2021-06-14 19:28:24 +0300

    feat: task_4 complite

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,30 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var prisesNew = prices
+        var profit = 0
+        var result = 0
+        
+        if prices.count > 0 {
+           for  indexNumMin in 1..<prisesNew.count  {
+                let numMin = prisesNew.reduce(Int.max, { min($0, $1) })
+                let numMax = prisesNew.reduce(Int.min, { max($0, $1) })
+                let indexNumMin = prisesNew.firstIndex(of: numMin)
+                let indexNumMax = prisesNew.firstIndex(of: numMax)
+                if indexNumMax! > indexNumMin! {
+                    profit = numMax - numMin
+                    prisesNew.remove(at: indexNumMin!)
+                }
+                else {
+                    prisesNew.remove(at: indexNumMax!)
+                    profit = 0
+                }
+                result += profit
+            }
+           
+        }
+        return result
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,51 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var message = message
+        while message.contains("[") {
+            decode(&message)
+        }
+        return message
+    }
+
+    private func decode(_ message: inout String) {
+        guard let lastIndex = message.firstIndex(of: "]"),
+              let firstIndex = message[...lastIndex].lastIndex(of: "["),
+              firstIndex != message.startIndex
+        else {
+            message = message
+                .replacingOccurrences(of: "[", with: "")
+                .replacingOccurrences(of: "]", with: "")
+            return
+        }
+
+        var firstNumIndex: String.Index?
+        var lastNumIndex: String.Index?
+
+        if message[message.index(before: firstIndex)].isNumber {
+            firstNumIndex = message.index(before: firstIndex)
+            lastNumIndex = message.index(before: firstIndex)
+        }
+
+        while let first = firstNumIndex,
+              first != message.startIndex,
+              message[message.index(before: first)].isNumber {
+            firstNumIndex = message.index(before: first)
+        }
+
+        let startString = message.index(after: firstIndex)
+        let endString = message.index(before: lastIndex)
+        let string = String(message[startString...endString])
+        guard let first = firstNumIndex,
+              let last = lastNumIndex,
+              let coef = Int(message[first...last])
+        else {
+            message.replaceSubrange(firstIndex...lastIndex,
+                                    with: String(repeating: string, count: 1))
+            return
+        }
+        message.replaceSubrange(first...lastIndex,
+                                with: String(repeating: string, count: coef))
     }
 }
+ 
